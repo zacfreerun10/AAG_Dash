2923b6041600d428fcefe8043c742ae0dbd258a4
zacfreerun10 <zacfreerun10@vt.edu>
2025-02-25 11:24:44 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["https://67bdda3bcb4eaa7c12ae6fd1--enchanting-chimera-2c76b0.netlify.app"],
+    allow_origins=["https://enchanting-chimera-2c76b0.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
